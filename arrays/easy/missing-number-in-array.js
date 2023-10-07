97d9a728fa0e9e7bc4d17a7f72a821023825cf3b
Gautam <gomzekh@gmail.com>
2023-10-08 02:42:05 +0530

    moved the code in correct file

--- arrays/easy/missing-number-in-array.js
+++ arrays/easy/missing-number-in-array.js
@@ -1,18 +1,31 @@
 /**
- Given an unsorted array A of size N that contains only positive integers, 
- find a continuous sub-array that adds to a given number S and return the 
- left and right index(1-based indexing) of that subarray.
-
-In case of multiple subarrays, return the subarray indexes which come first 
-on moving from left to right.
-
-Note:- You have to return an ArrayList consisting of two elements left and right. 
-In case no such subarray exists return an array consisting of element -1.
+ * Given an unsorted array A of size N that contains only positive integers, 
+ * find a continuous sub-array that adds to a given number S and return the 
+ * left and right index(1-based indexing) of that subarray.
+ * 
+ * In case of multiple subarrays, return the subarray indexes which come first 
+ * on moving from left to right.
+ * 
+ * Note:- You have to return an ArrayList consisting of two elements left and right. 
+ * In case no such subarray exists return an array consisting of element -1.
+ * 
+ * Input:
+ * N = 5, S = 12
+ * A[] = {1,2,3,7,5}
+ * 
+ * Output: 2 4
+ * 
+ * Explanation: The sum of elements 
+ * from 2nd position to 4th position 
+ * is 12.
 
  * @param {number[]} arr
  * @param {number} n
  * @param {number} s
  * @returns {number[]}
+ * 
+ * Expected Time Complexity: O(N)
+ * Expected Auxiliary Space: O(1)
  */
 
 class Solution {
